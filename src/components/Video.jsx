fdc445a5bb85727fd50cf2a528ef7c0f99ee4a95
Mehedi Bhuiyan <mehedibuiyan734@gmail.com>
2023-01-12 02:26:13 +0600

    fetch videos from database

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -1,18 +1,17 @@
-import React from "react";
-import image from "../assist/3.jpg";
-import styles from "../styles/Video.module.css";
-import { Link } from "react-router-dom";
-const Video = () => {
+import classes from "../styles/Video.module.css";
+
+export default function Video({ title, id, noq }) {
   return (
-    <div className={styles.video}>
-      <img src={image} alt="" />
-      <p>#23 React Hooks Bangla - React useReducer hook Bangla</p>
-      <div className={styles.qmeta}>
-        <p>10 Questions</p>
-        <p>Score : Not taken yet</p>
+    <div className={classes.video}>
+      <img
+        src={`http://img.youtube.com/vi/${id}/maxresdefault.jpg`}
+        alt={title}
+      />
+      <p>{title}</p>
+      <div className={classes.qmeta}>
+        <p>{noq} Questions</p>
+        <p>Total points : {noq * 5}</p>
       </div>
     </div>
   );
-};
-
-export default Video;
+}
--- src/components/pages/Quiz.jsx
+++ src/components/pages/Quiz.jsx
@@ -1,14 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
+import { useParams } from "react-router-dom";
 import Answers from "../Answers";
 import Miniplayer from "../Miniplayer";
 import Progressbar from "../Progressbar";
+import useQuestionsList from "../hooks/useQuestionsList";
+
 const Quiz = () => {
+  const { id } = useParams();
+  const [currentQuestions, setCurrentQuestions] = useState();
+  const { loading, error, questions } = useQuestionsList(id);
   return (
     <div className="container">
       <h1>Pick three of your favorite Star Wars Flims</h1>
       <h4>Question can have multiple answers</h4>
-      <Answers />
-      <Answers />
       <Answers />
       <Answers />
       <Progressbar />
